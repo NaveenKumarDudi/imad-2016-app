88bf26c5c53004611e75bf27b34876fbcb99050b
naveenkumardudi <naveen.14jics057@jietjodhpur.ac.in>
2016-10-04 21:44:22 +0530

    Further ui changes

--- server.js
+++ server.js
@@ -22,6 +22,12 @@ app.get('/ui/logo.png', function (req, res) {
 app.get('/ui/1.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', '1.jpg'));
 });
+app.get('/ui/resume.jpeg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'resume.jpeg'));
+});
+app.get('/ui/resume1.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'resume1.jpg'));
+});
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
