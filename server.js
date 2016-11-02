cad5424b36ff47bbd40e5422fbbf637f16e2e507
naveenkumardudi <naveen.14jics057@jietjodhpur.ac.in>
2016-11-02 22:02:09 +0530

    New Idea

--- server.js
+++ server.js
@@ -5,62 +5,51 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+//To get index page
 
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-app.get('/ui/max.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'max.css'));
-});
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-app.get('/ui/web.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'web.jpg'));
-});
-app.get('/ui/web1.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'web1.png'));
-});
-app.get('/ui/cp.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'cp.png'));
-});
-app.get('/ui/python.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'python.png'));
-});
-app.get('/ui/java.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'java.png'));
-});
-app.get('/ui/cpp.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'cpp.png'));
-});
-app.get('/ui/wb.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'wb.jpg'));
+
+app.get('/about.html',function(req,res){
+	res.sendFile(path.join(__dirname,'ui','about.html'));
 });
 
-app.get('/ui/c.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'c.jpg'));
+app.get('/',function(req,res){
+	res.sendFile(path.join(__dirname,'ui','index.html'));
 });
 
-app.get('/ui/logo.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'logo.png'));
+//to get view article page
+app.get('/article.html',function(req,res){
+	res.sendFile(path.join(__dirname,'ui','article.html'));
 });
-app.get('/ui/1.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', '1.jpg'));
+
+//to include css file
+
+app.get('/style.css',function(req,res){
+	res.sendFile(path.join(__dirname,'ui/css','style.css'));
 });
-app.get('/ui/resume.jpeg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'resume.jpeg'));
+
+// to include javascript files
+
+app.get('/main.js',function(req,res){
+	res.sendFile(path.join(__dirname,'ui/js','main.js'));
 });
-app.get('/ui/resume1.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'resume1.jpg'));
+
+// to include images from image folder
+
+app.get('/1.jpg',function(req,res){
+	res.sendFile(path.join(__dirname,'ui/images','1.jpg'));
 });
-app.get('/ui/nav.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'nav.jpg'));
+
+app.get('/background.png',function(req,res){
+	res.sendFile(path.join(__dirname,'ui/images','background.png'));
 });
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+
+app.get('/logo.png',function(req,res){
+	res.sendFile(path.join(__dirname,'ui/images','logo.png'));
+});
+
+//To run Server on following Port
+var port = 8080;
+app.listen(8080,function(){
+	console.log(`Server Running on port ${port}`);
 });
